934a6c97ba18e44fbd03c177c141819f74ac731a
erlandego <50873837+erlandego@users.noreply.github.com>
2023-05-29 03:09:02 +0700

    Merk edit selesai

--- app/Http/Controllers/DashboardMerkController.php
+++ app/Http/Controllers/DashboardMerkController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MerkCategory;
 use App\Models\Merk;
+use App\Models\MerkCategory;
 use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -65,49 +65,139 @@ class DashboardMerkController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\MerkCategory  $merkCategory
+     * @param  \App\Models\Merk  $merk
      * @return \Illuminate\Http\Response
      */
-    public function show(MerkCategory $merkCategory)
+    public function show(Merk $merk)
     {
-        return view('dashboard.merk.merk' , [
-            'page' => 'List Merk',
-            'title' => 'Admin | List Merk',
-            'listmerkcat' => MerkCategory::orderBy('category_id' , 'asc')->get() ,
-            'listmerk' => Merk::all()
-        ]);
+        // return view('dashboard.merk.edit' , [
+        //     "page" => 'List Merk',
+        //     "title" => 'Admin | Edit Merk',
+        //     'listmerk' => Merk::all(),
+        //     'listcategory' => Category::all(),
+        //     'merkcat' => MerkCategory::all(),
+        //     'merk' => $merk
+        // ]);
+
+        //return $merk;
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\MerkCategory  $merkCategory
+     * @param  \App\Models\Merk  $merk
      * @return \Illuminate\Http\Response
      */
-    public function edit(MerkCategory $merkCategory)
+    public function edit(Merk $merk)
     {
-        //
+        return view('dashboard.merk.edit' , [
+            "page" => 'List Merk',
+            "title" => 'Admin | Edit Merk',
+            'listmerk' => Merk::all(),
+            'listcategory' => Category::all(),
+            'merkcat' => MerkCategory::all(),
+            'merk' => $merk
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\MerkCategory  $merkCategory
+     * @param  \App\Models\Merk  $merk
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, MerkCategory $merkCategory)
+    public function update(Request $request, Merk $merk)
     {
-        //
+        $merkcat = MerkCategory::where('merk_id' , $merk->id)->get();
+        $catpilihan = $request->category;
+        $hapus = [];
+        $tambah = [];
+
+        // $validatedData = $request->validate([
+        //     'nama_merk' => 'required'
+        // ]);
+
+        //cek 1
+        foreach ($merkcat as $value) {
+            $cek = false;
+            foreach ($catpilihan as $item) {
+                if($item == $value->category_id){
+                    $cek = true;
+
+                }
+            }
+            if($cek == false){
+                array_push($hapus , $value->id);
+            }
+        }
+
+        $count = 1;
+        //cek2
+        foreach ($catpilihan as $value) {
+            $cek = false;
+            foreach ($merkcat as $item) {
+                if($value == $item->category_id){
+                    $cek = true;
+                    $count++;
+                }
+            }
+            if($cek == false){
+                array_push($tambah , $value);
+            }
+        }
+
+        print_r($tambah);
+        print "<br>";
+        print_r($hapus);
+        print "<br>";
+        print_r($catpilihan);
+
+        // Merk::where('id' , $merk->id)->update($validatedData);
+        // if(count($merkcat) > count($catpilihan)){
+        //     foreach ($merkcat as  $value) {
+        //         $cek = false;
+        //         foreach ($catpilihan as $item) {
+        //             if($value->category_id == $item){
+        //                 $cek = true;
+        //                 array_push($ada , $value->id);
+        //             }
+        //         }
+        //         if($cek == false){
+        //             array_push($tidakada , $value->id);
+        //         }
+        //     }
+        // }
+        // else if(count($merkcat) < count($catpilihan)){
+        //     foreach ($catpilihan as $value) {
+        //         $cek = false;
+        //         foreach($merkcat as $item){
+        //             if($value == $item->id){
+        //                 $cek = true;
+        //                 array_push($ada , $item->id);
+        //             }
+        //         }
+        //         if($cek == false){
+        //             array_push($tidakada , $value->id);
+        //         }
+        //     }
+        // }
+        // else if(count($merkcat) == count($catpilihan)){
+        //     foreach ($merkcat as $value) {
+        //         # code...
+        //     }
+        // }
+
+        //return $ada;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\MerkCategory  $merkCategory
+     * @param  \App\Models\Merk  $merk
      * @return \Illuminate\Http\Response
      */
-    public function destroy(MerkCategory $merkCategory)
+    public function destroy(Merk $merk)
     {
         //
     }
--- resources/views/dashboard/merk/edit.blade.php
+++ resources/views/dashboard/merk/edit.blade.php
@@ -1,13 +1,15 @@
 @extends('dashboard.layout.dashboard')
 
 @section('container')
-    <form method="post" action="/dashboard/merk">
+    @php $cari = 0; @endphp
+    <form method="post" action="/dashboard/merk/{{ $merk->id }}">
     @csrf
+    @method('put')
     {{-- Nama Merk --}}
     <div class="divnama mb-3">
         <div class="form-group">
             <b><label for="nama_merk">Nama Merk</label></b>
-            <input type="text" class="form-control" id=nama_merk name="nama_merk" value="{{ old('nama_merk') }}" required>
+            <input type="text" class="form-control" id=nama_merk name="nama_merk" value="{{ $merk->nama_merk }}" required>
         </div>
         @error('nama_merk')
             <div class="invalid-feedback">
@@ -19,13 +21,19 @@
     {{-- Category Merk --}}
     <div class="form-group mb-3">
         <b><label>Category</label></b>
-        @foreach ($category as $item)
+        @foreach ($listcategory as $item)
+            @foreach($merkcat as $itm)
+                @if($itm->merk_id == $merk->id && $itm->category_id == $item->id)
+                    @php $cari = 1 @endphp
+                @endif
+            @endforeach
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="category[]" value="{{ $item->id }}" id="{{ $item->name }}">
+                <input class="form-check-input" type="checkbox" name="category[]" value="{{ $item->id }}" id="{{ $item->name }}" @if($cari == 1) checked @endif>
                 <label class="form-check-label" for="{{ $item->name }}">
-                      {{ $item->name }}
+                        {{ $item->name }}
                 </label>
             </div>
+            @php $cari = 0 @endphp
         @endforeach
         @error('category')
             <div class="invalid-feedback">
@@ -34,6 +42,6 @@
         @enderror
     </div>
 
-    <button type="submit" class="btn btn-primary">Tambah +</button>
+    <button type="submit" class="btn btn-warning">Edit +</button>
     </form>
 @endsection
